5a4f7f18fcca7471a8c544044fefec17d80910da
minicatsCB <maribeldiaz96@yahoo.es>
2017-05-09 15:44:47 +0200

    First mapper try

---
+++ m1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # split the line into words
+    words = line.split(",")
+    # increase counters
+    for word in words:
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        print '%s' % (word)
+
