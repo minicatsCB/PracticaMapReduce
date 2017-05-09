85d86a4373f22ef883e1002ec7b87f67f00dd203
minicatsCB <maribeldiaz96@yahoo.es>
2017-05-09 16:56:06 +0200

    Check for incorrect number of attributes

--- m1.py
+++ m1.py
@@ -2,14 +2,30 @@
 
 import sys
 
+j = 0
 # input comes from STDIN (standard input)
 for line in sys.stdin:
-    # split the line into words
-    words = line.split(",")
-    # increase counters
-    for word in words:
-        # write the results to STDOUT (standard output);
-        # what we output here will be the input for the
-        # Reduce step, i.e. the input for reducer.py
-        print '%s' % (word)
+	# split the line into words
+	words = line.split(",")
+
+	# Check if the line has a different number of attributes (in this case, 31)
+	# If that occurs, skip the line
+	if(len(words) != 31):
+		print("Different number of attributes in line " + str(j))
+		j = j + 1
+		continue;
+
+	# increase counters
+	i = 0 # Number of words that has been processed
+	for word in words:
+		# write the results to STDOUT (standard output);
+		# what we output here will be the input for the
+		# Reduce step, i.e. the input for reducer.py
+		#print("---------------------------- Palabra " + str(i) + " ------------------------")
+		#print '%s' % (word)
+		if(i == 30):
+			#print("--- Linea " + str(j) + " ---")
+			pass
+		i = i + 1
+	j = j + 1
 
