f28415d444d3b98155cfd74e364ee022fb89077f
rubenamador <ruben.amador@live.u-tad.com>
2017-05-19 14:09:42 +0200

    Arreglando comentarios

--- r3.py
+++ r3.py
@@ -35,7 +35,7 @@ for line in sys.stdin:
 	
 	'''Recorremos la tupla por columnas'''
 	for i in range(len(words)):
-		'''Nos saltamos las 2 primeras columnas que tienen strings'''
+		'''Nos saltamos la primera columna porque tiene string'''
 		if(i > 0):
 			'''Vamos sumando el trafico de cada horario'''
 			traffic_num += float(words[i].replace('[','').replace(']','')
