16ff20b52a337cd6a52c78ed59ed52bf7bfb7156
rubenamador <ruben.amador@live.u-tad.com>
2017-05-25 14:27:11 +0200

    Añadiendo cambios para no utilizar listas
    Las listas no son eficientes, y por eso, se ha tenido que cambiar la forma de imprimir las consultas

--- r3.py
+++ r3.py
@@ -3,9 +3,6 @@
 from operator import itemgetter
 import sys
 
-traffic = []
-streets = []
-numbers = []
 wordBefore = None
 traffic_num = 0
 most_traffic_street = None
@@ -22,13 +19,17 @@ for line in sys.stdin:
 	'''Si el nombre de la calle es diferente'''
 	if(words[0] != str(wordBefore)):
 		#print(words[0] + ':' + str(wordBefore))
-		'''Guardamos el nombre de la calle a la lista de calles'''
-		streets.append(words[0])
 		'''Nos saltamos la primera iteracion porque todavia no tenemos ninguna 
 		calle anterior guardada'''
 		if(j > 0):
-			'''Guardamos trafico calculado en la lista de traficos'''
-			traffic.append(traffic_num)
+			'''Si el trafico actual es mayor que trafico mas alto hasta el momento'''
+			if(traffic_num > most_traffic_num):
+				'''Guardamos el nombre de la calle'''
+				most_traffic_street = wordBefore
+				'''Guardamos el numero de trafico'''
+				most_traffic_num = traffic_num
+			'''Vamos acumulando el trafico de cada calle en una variable'''
+			traffic_total += traffic_num
 			'''Reiniciamos el calculo para la calle siguiente'''
 			traffic_num = 0
 		j = j + 1
@@ -44,17 +45,6 @@ for line in sys.stdin:
 	
 	'''Guardamos la calle actual en la calle anterior'''
 	wordBefore = words[0]
-
-'''Recorremos la lista de traficos'''
-for i in range(len(traffic)):
-	'''Si el trafico actual es mayor que trafico mas alto hasta el momento'''
-	if(traffic[i] > most_traffic_num):
-		'''Guardamos el nombre de la calle'''
-		most_traffic_street = streets[i]
-		'''Guardamos el numero de trafico'''
-		most_traffic_num = traffic[i]
-	'''Vamos acumulando el trafico de cada calle en una variable'''
-	traffic_total += traffic[i]
 
 print("Trafico total:" + str(traffic_total))
 print("Calle con mas trafico:" + most_traffic_street.replace('[',''))
